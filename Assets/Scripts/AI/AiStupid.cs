606fc1a44ae59967bbcc76562b414e5c59dbe6dc
Josh W. <joshuawooten04@gmail.com>
2024-10-22 17:39:32 -0700

    Add files via upload

---
+++ Assets/Scripts/AI/AiStupid.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class AiStupid : AIController
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+    }
+    public override void MakeDecisions()
+    {
+        base.MakeDecisions();
+        switch(currentState)
+        {
+            case AIState.choose:
+            TargetPlayerOne();
+            ChangeState(AIState.Idle);
+            break;
+
+            case AIState.Idle:
+            DoIdleState();
+            if (IsDisntanceLessThan(target, 5))
+            {
+                ChangeState(AIState.patrol);
+            }
+            break;
+
+            case AIState.patrol:
+            patrol();
+            if(IsDisntanceLessThan(target, 4))
+            {
+            ChangeState(AIState.Attack);
+            }
+            break;
+
+            case AIState.Attack:
+            DoAttackState();
+            ChangeState(AIState.patrol);
+            break;
+        }
+    }
+}
