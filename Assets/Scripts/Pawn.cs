0825521ff0f3a44b18fb81e08904abbc3885fe5d
Josh W. <joshuawooten04@gmail.com>
2024-10-22 17:47:25 -0700

    Update Pawn.cs

--- Assets/Scripts/Pawn.cs
+++ Assets/Scripts/Pawn.cs
@@ -9,17 +9,25 @@ public abstract class Pawn : MonoBehaviour
     public float turnSpeed;
     //storing our mover. 
     public Mover mover; 
+    public NoiseMaker noiseMaker;
+    public float movingVolumeDistance;
 
     //when the game starts, it will get the mover to obtain the Mover copmonent 
     public virtual void Start()
     {
         mover = GetComponent<Mover>();
+        shooter = GetComponent<Shooter>();
+        noiseMaker = GetComponent<NoiseMaker>();
     }
 
     // needs to be here and public so TankPawn can get it even if theres nothing in it
     public virtual void Update()
     {
     }
+    public GameObject ShellPrefab;
+    public float fireForce;
+    public float damageDone;
+    public float ShellLifespan;
 
     //scripts for the move commands
     public abstract void MoveForward();
